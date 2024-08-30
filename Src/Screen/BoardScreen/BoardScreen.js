b7cbe644c8ea0d2a037a6b5a6e097c0510c51f0e
nitin-tec-sense <nitin@tec-sense.com>
2024-08-30 11:37:17 +0530

    add board top button

--- Src/Screen/BoardScreen/BoardScreen.js
+++ Src/Screen/BoardScreen/BoardScreen.js
@@ -7,7 +7,8 @@ import {
     TouchableOpacity,
     Image,
     FlatList,
-    Dimensions, Alert, ScrollView
+    Dimensions, Alert, ScrollView,
+    StyleSheet
 } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { COLOR } from '../../Assets/AllFactors/AllFactors';
@@ -16,7 +17,7 @@ import { getToken } from '../../Service/AsyncStorage';
 import { Task_Done, Task_User_List } from '../../Service/actions';
 import Loader from '../../Custom/Loader/loader';
 const BoardScreen = () => {
-    const [today, setToday] = useState('Screen');
+    const [isAcctive, setIsAcctive] = useState('Screen');
     const [isFocus, setIsFocus] = useState('Receive');
     const [token, setToken] = useState('');
     const [userData, setAllUserData] = useState([]);
@@ -73,6 +74,7 @@ const BoardScreen = () => {
 
         return (
             <View style={{ backgroundColor: COLOR.white, paddingHorizontal: 5, marginTop: 5, paddingVertical: 5 }}>
+
                 <View
                     style={{
                         padding: 8,
@@ -123,7 +125,21 @@ const BoardScreen = () => {
                     borderRadius: 20,
                     paddingTop: 15
                 }}>
+                <View style={{ height: 100, flex: 1, paddingVertical: 10, paddingHorizontal: 10, justifyContent: 'center', flexDirection: 'row' }}>
+                    <TouchableOpacity style={[styles.topButton, { backgroundColor: isAcctive == 'Screen' ? COLOR.green : COLOR.white }]} onPress={() => { setIsAcctive("Screen") }}>
+                        <Title name={'Screen'} color={isAcctive == 'Screen' ? COLOR.white : COLOR.black} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.topButton, { backgroundColor: isAcctive == 'Holidays' ? COLOR.green : COLOR.white }]} onPress={() => { setIsAcctive("Holidays") }}>
+                        <Title name={'Holidays'} color={isAcctive == 'Holidays' ? COLOR.white : COLOR.black} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.topButton, { backgroundColor: isAcctive == 'Meetings' ? COLOR.green : COLOR.white }]} onPress={() => { setIsAcctive("Meetings") }}>
+                        <Title name={'Meetings'} color={isAcctive == 'Meetings' ? COLOR.white : COLOR.black} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.topButton, { backgroundColor: isAcctive == 'Events' ? COLOR.green : COLOR.white }]} onPress={() => { setIsAcctive("Events") }}>
+                        <Title name={'Events'} color={isAcctive == 'Events' ? COLOR.white : COLOR.black} />
+                    </TouchableOpacity>
 
+                </View>
                 <CalendarView />
                 {/* <DropDown Month={'Today'} onPress={() => Alert.alert('today')} /> */}
                 <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf: 'center', marginTop: 30 }}>
@@ -136,10 +152,8 @@ const BoardScreen = () => {
                 </View>
                 <FlatList
                     style={{ marginBottom: 100 }}
-
                     renderItem={list}
                     data={userData}
-
                     bounces={false}
                 />
 
@@ -166,5 +180,15 @@ const DropDown = ({ Month, onPress, isshow }) => {
         </TouchableOpacity>
     )
 }
+const Title = ({ name, color }) => {
+    return (
+        <Text style={{ fontSize: 16, fontWeight: 'bold', color: color }}>{name}</Text>
+    )
+}
 
 export default BoardScreen;
+const styles = StyleSheet.create({
+    topButton:
+        { flex: 0.25, backgroundColor: 'white', margin: 3, justifyContent: 'center', alignItems: 'center', shadowOpacity: 0.2, shadowOffset: { height: 1, width: 2 }, shadowRadius: 5, borderRadius: 10 }
+
+})
