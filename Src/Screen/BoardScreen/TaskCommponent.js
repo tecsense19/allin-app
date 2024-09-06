8662098517f0c593a57323e2d73bc2f18717a48f
nitin-tec-sense <nitin@tec-sense.com>
2024-09-06 13:01:09 +0530

    Create TaskCommponent.js

---
+++ Src/Screen/BoardScreen/TaskCommponent.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TaskCommponent = () => {
+    return (
+        <View>
+            <Text>TaskCommponent</Text>
+        </View>
+    )
+}
+
+export default TaskCommponent
