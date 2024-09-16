51ec3f49ecf641659209fd1ebef24170b424458a
nitin-tec-sense <nitin@tec-sense.com>
2024-09-16 19:05:50 +0530

    Update GroupChatScreen.js

--- Src/Screen/GroupChatScreen/GroupChatScreen.js
+++ Src/Screen/GroupChatScreen/GroupChatScreen.js
@@ -1,13 +1,35 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { User_List_For_Group } from '../../Service/actions';
+import { getToken } from '../../Service/AsyncStorage';
+import Loader from '../../Custom/Loader/loader';
 
 const GroupChatScreen = (props) => {
     const groupId = props.route.params
-    console.log(groupId);
+    const [userData, setUserData] = useState([])
+    const [loading, setLoading] = useState(false)
+
+
+    useEffect(() => {
+        onFatchUserList()
+    }, [])
+
+    const onFatchUserList = async () => {
+        setLoading(true)
+        const token = await getToken()
+        User_List_For_Group(token, groupId)
+            .then((res) => {
+                if (res.status_code == 200) {
+                    setLoading(false)
+                    setUserData(res.data.user_list)
+                }
+            })
+    }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text style={{ fontSize: 20, fontWeight: 'bold' }}>GroupChatScreen</Text>
+            <Loader visible={loading} />
         </View>
     )
 }
