2f98cc48ff8f6668f19a8072d0dd79345b1d05df
nitin-tec-sense <nitin@tec-sense.com>
2024-09-16 19:21:48 +0530

    Update MeetingCommponent.js

--- Src/Screen/BoardScreen/MeetingCommponent.js
+++ Src/Screen/BoardScreen/MeetingCommponent.js
@@ -34,7 +34,7 @@
 //         }
 //         const member = item?.assigned_users
 //         const time12HourString = convertTime24To12(item.start_time);
-//         const dateTime12HourString = convertDateTimeTo12HourFormat(item.created_at);
+//         const MsgTime = convertDateTimeTo12HourFormat(item.created_at);
 //         return (
 //             <View style={styles.meetinglistContainer}>
 //                 <View style={{ backgroundColor: COLOR.black, height: 40, justifyContent: 'center', borderTopRightRadius: 10, borderTopLeftRadius: 10 }}>
@@ -65,7 +65,7 @@
 //                     <View style={{ flexDirection: 'row', alignItems: 'center', }}>
 
 //                     </View>
-//                     <Text style={styles.time}>{dateTime12HourString}</Text>
+//                     <Text style={styles.time}>{MsgTime}</Text>
 //                 </View>
 //                 <View style={{ width: item.assigned_users.length <= 2 ? 60 : 75, alignSelf: 'center', flexDirection: 'row', alignItems: 'center', right: item.assigned_users.length <= 2 ? -20 : 0, bottom: 35 }}>
 //                     {member?.map((i, ind) => {
@@ -108,7 +108,7 @@
 
 // })
 
-import { View, Text, FlatList, StyleSheet, Image } from 'react-native'
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getToken } from '../../Service/AsyncStorage';
 import { Meetings } from '../../Service/actions';
@@ -142,43 +142,77 @@ const MeetingCommponent = () => {
             const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
             return `${date} ${hours12}:${formattedMinutes} ${period}`;
         }
+        const formatDate = (dateString) => {
+            const date = new Date(dateString);
+            return date.toLocaleDateString('en-US', {
+                weekday: 'long', // full name of the day (e.g., Sunday)
+                day: 'numeric',  // numeric day (e.g., 6)
+                month: 'short',  // short form of the month (e.g., Oct)
+                year: 'numeric', // numeric year (e.g., 2024)
+            });
+        };
         const member = item?.assigned_users
-        const time12HourString = convertTime24To12(item.start_time);
-        const dateTime12HourString = convertDateTimeTo12HourFormat(item.created_at);
+        const meetingtime12HourString = convertTime24To12(item.start_time);
+        const MsgTime = convertDateTimeTo12HourFormat(item.created_at)
+        const MeetingDate = formatDate(item.date)
+
+
         return (
             <View style={styles.meetinglistContainer}>
                 <Text style={styles.meetingTxt}>Meeting</Text>
+                <View style={{ height: 1.5, backgroundColor: COLOR.lightgray, marginHorizontal: 15, marginTop: 5 }}></View>
                 <View style={styles.meetingDetailsView}>
-                    <Text style={styles.Heading}>Title: <Text style={styles.HeadingContent}>{item.title}</Text></Text>
-                    <Text style={styles.Heading}>Description:
-                        <Text style={styles.HeadingContent}>{item.description?.length > 60 ? item.description.slice(0, 60) + '....' : item.description}</Text>
-                    </Text>
-                    <Text style={styles.Heading}>Meeting Date:
-                        <Text style={styles.HeadingContent}>{item.date}</Text>
-                    </Text>
-                    <Text style={styles.Heading}>Meeting Time:
-                        < Text style={styles.HeadingContent}>{time12HourString}</Text>
-                    </Text>
-                    <Text style={styles.Heading}>Location:
-                        < Text style={styles.HeadingContent}>{item.location ? item.location : 'Empty'}</Text>
-                    </Text>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', }}>
-                        <View style={{ width: item.assigned_users.length <= 2 ? 60 : 75, alignSelf: 'center', flexDirection: 'row', alignItems: 'center', position: 'absolute', right: item.assigned_users.length <= 2 ? -20 : 0, bottom: 35 }}>
-                            {member?.map((i, ind) => {
-                                return (
-                                    <View>
-                                        {ind < 3 ? <Image source={{ uri: i?.profile == null ? 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-1024.png' : 'https://allin.website4you.co.in/public/user-profile/' + i?.profile }} style={{
-                                            height: 25, width: 25,
-                                            borderRadius: 100, marginLeft: ind == 0 ? 0 : -10,
-                                        }} /> : ''}
-                                    </View>
-                                )
-                            })}
-                            <Text style={styles.assignedUser}>{item.assigned_users?.length >= 3 ? (item.assigned_users?.length - 3 == 0 ? '' : '+' + (item.assigned_users?.length - 3)) : ""}</Text>
-                        </View>
+                    <Text style={{ fontSize: 15, color: COLOR.black, fontWeight: 'bold', marginTop: 5 }}>{item.title}</Text>
+
+                    <Text style={{ fontSize: 13, marginTop: 5, color: COLOR.gray, fontWeight: '600', }}>{item.description?.length > 100 ? item.description.slice(0, 100) + '....' : item.description}</Text>
+                    <View style={{ height: 1.5, backgroundColor: COLOR.lightgray, marginTop: 10, }}></View>
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+                        <Image source={require('../../Assets/Image/clock.png')} style={{ height: 17, width: 17, resizeMode: 'contain' }} />
+                        <Text style={{ fontSize: 13, color: COLOR.gray, fontWeight: '600', marginLeft: 5 }}>{MeetingDate}</Text>
+                        <View style={{ height: 5, width: 5, backgroundColor: COLOR.lightgray, borderRadius: 10, marginLeft: 10 }}></View>
+                        <Text style={{ fontSize: 13, color: COLOR.gray, fontWeight: '600', marginLeft: 10 }}>{meetingtime12HourString}</Text>
+
                     </View>
-                    <Text style={styles.time}>{dateTime12HourString}</Text>
+
+                    <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
+                        <Image source={require('../../Assets/Image/location.png')} style={{ height: 20, width: 20, resizeMode: 'contain', marginLeft: -2 }} />
+                        < Text style={{ fontSize: 13, color: COLOR.gray, fontWeight: '600', marginRight: 15, marginLeft: 3 }}>{item.location ? item.location : 'Empty'}</Text>
+
+                    </View>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginTop: 15, marginHorizontal: 10 }}>
+                        <TouchableOpacity style={{ backgroundColor: COLOR.green, height: 30, borderRadius: 30, width: '40%', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', paddingHorizontal: 15, marginVertical: 10 }}>
+                            <Text style={{ color: COLOR.white, fontWeight: '600', fontSize: 15 }}>Accept</Text>
+                            <Image source={require('../../Assets/Image/accept.png')} style={{ height: 20, width: 20, resizeMode: 'contain', marginLeft: -2, tintColor: COLOR.white }} />
+
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ borderWidth: 1, borderColor: COLOR.green, height: 30, borderRadius: 30, width: '40%', flexDirection: 'row', paddingHorizontal: 15, justifyContent: 'space-around', alignItems: 'center' }}>
+                            <Text style={{ color: COLOR.green, fontSize: 15, fontWeight: '600' }}>Decline</Text>
+                            <Image source={require('../../Assets/Image/x.png')} style={{ height: 13, width: 13, resizeMode: 'contain', tintColor: COLOR.green }} />
+
+                        </TouchableOpacity>
+                    </View>
                 </View>
+                <Text style={{ fontSize: 13, color: COLOR.black, fontWeight: 'bold', marginLeft: 15, marginTop: 10 }}>{'Assigned Task :' + member.length}</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 15, marginTop: 10, justifyContent: 'space-between', marginBottom: 5 }}>
+                    <View style={{ alignSelf: 'center', flexDirection: 'row', alignItems: 'center', }}>
+                        {member?.map((i, ind) => {
+                            return (
+                                <View>
+                                    {ind < 3 ? <Image source={{ uri: i?.profile == null ? 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-1024.png' : 'https://allin.website4you.co.in/public/user-profile/' + i?.profile }} style={{
+                                        height: 30, width: 30,
+                                        borderRadius: 100, marginLeft: ind == 0 ? 0 : -10,
+                                    }} /> : ''}
+                                </View>
+                            )
+                        })}
+
+                        {/* <Text style={styles.assignedUser}>{item.assigned_users?.length >= 3 ? (item.assigned_users?.length - 3 == 0 ? '' : '+' + (item.assigned_users?.length - 3)) : ""}</Text> */}
+                    </View>
+                    <Text style={styles.time}>{MsgTime}</Text>
+
+                </View>
+
             </View>
         )
     }
@@ -198,11 +232,11 @@ export default MeetingCommponent
 const styles = StyleSheet.create({
     Heading: { alignSelf: 'flex-start', marginTop: 10, fontWeight: 'bold', color: COLOR.black, fontSize: 13 },
     HeadingContent: { color: COLOR.black, fontSize: 12, fontWeight: '500' },
-    time: { alignSelf: 'flex-end', margin: 5, fontWeight: '700', color: COLOR.placeholder, fontSize: 12 },
+    time: { alignSelf: 'flex-end', margin: 5, fontWeight: '700', color: COLOR.placeholder, fontSize: 12, marginRight: 15 },
     meetingFlatList: { marginHorizontal: 20, paddingBottom: 120 },
-    meetingTxt: { fontSize: 18, fontWeight: 'bold', marginBottom: 5, color: COLOR.black, marginLeft: 5 },
-    meetinglistContainer: { padding: 5, backgroundColor: COLOR.lightgreen, margin: 5 },
-    meetingDetailsView: { height: 'auto', borderRadius: 10, paddingHorizontal: 10, padding: 5, paddingLeft: 15 },
+    meetingTxt: { fontSize: 18, fontWeight: 'bold', marginBottom: 5, color: COLOR.black, marginLeft: 15, marginTop: 5 },
+    meetinglistContainer: { padding: 5, backgroundColor: COLOR.white, margin: 5, borderRadius: 10, shadowOffset: { height: 2, width: 1 }, shadowOpacity: 0.2, shadowRadius: 3, marginTop: 20 },
+    meetingDetailsView: { height: 'auto', borderRadius: 10, paddingHorizontal: 15, padding: 5, paddingLeft: 15 },
     assignedUser: { fontSize: 12, fontWeight: 'bold', color: COLOR.titlecolor, },
 
 })
