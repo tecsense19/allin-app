f67540ce36c2bee311e703a7412ddafcbf3368a9
nitin-tec-sense <nitin@tec-sense.com>
2024-09-27 10:24:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { View, LogBox, TouchableOpacity, Image } from 'react-native'
+import { View, LogBox, TouchableOpacity, Image, } from 'react-native'
 import React, { useEffect } from 'react'
 import StackScreen from './Src/Navigation/Stack/Stcak'
 import { Provider } from 'react-redux';
@@ -8,7 +8,6 @@ import messaging from '@react-native-firebase/messaging';
 
 LogBox.ignoreAllLogs();
 const App = () => {
-
   // useEffect(() => { getFcmToken() }, [])
   // const getFcmToken = async () => {
   //   try {
@@ -28,60 +27,3 @@ const App = () => {
   )
 }
 export default App
-
-// import { View, Text } from 'react-native'
-// import React, { useEffect, useState } from 'react'
-// import { User_List, User_Mobile_Number } from './Src/Service/actions'
-// import { getToken } from './Src/Service/AsyncStorage'
-// import { PERMISSIONS, request } from 'react-native-permissions'
-// import Contacts from 'react-native-contacts';
-
-
-// const App = () => {
-//   const [contactsData, setContacts] = useState([])
-//   const [ApiContact, setApiContact] = useState([])
-
-//   const getApiMobailNumbers = async () => {
-//     const token = await getToken()
-//     const data = await User_List('', token)
-//     console.log(data.data.userList);
-
-//   }
-//   const requestContactsPermission = async () => {
-//     const result = await request(PERMISSIONS.IOS.CONTACTS);
-//     if (result === 'granted') {
-//       Contacts.getAll().then(contacts => {
-//         setContacts(contacts)
-//         // console.log(contacts);
-
-//       })
-//     }
-//     else { customAlert(); }
-//   };
-//   const transformData = (data) => ({
-//     givenName: data.givenName,
-//     phoneNumbers: data.phoneNumbers.map(phone => phone.number)
-//   });
-//   const transformedDataArray = contactsData.map(transformData);
-//   useEffect(() => {
-//     getApiMobailNumbers(),
-//       requestContactsPermission()
-//   }, [])
-//   const formatPhoneNumbers = (contacts) => {
-//     return contacts.map(contact => {
-//       return contact.phoneNumbers.map(number =>
-//         number.replace(/\D/g, '')  // Remove non-digit characters
-//       );
-//     }).flat();
-//   }
-
-//   const formattedNumbers = formatPhoneNumbers(transformedDataArray);
-//   console.log(formattedNumbers);
-//   return (
-//     <View>
-//       <Text>App</Text>
-//     </View>
-//   )
-// }
-
-// export default App
