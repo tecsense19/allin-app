44d7c22bd6f79837f095895bc4253cfb30065de3
tec-sense <tecsense19@gmail.com>
2024-12-02 18:29:23 +0530

    Update EventsCommponent.js

--- Src/Screen/BoardScreen/EventsCommponent.js
+++ Src/Screen/BoardScreen/EventsCommponent.js
@@ -9,7 +9,12 @@ import Loader from '../../Custom/Loader/loader'
 const EventsCommponent = ({ onPress }) => {
     const [EventData, setEventData] = useState([])
     const [loading, setLoading] = useState(false)
+    const [myid, setMyId] = useState('')
 
+    const getMyId = async () => {
+        const myId = await MyID()
+        setMyId(myId)
+    }
     const getEventData = async () => {
         setLoading(true)
         const token = await getToken()
@@ -36,11 +41,11 @@ const EventsCommponent = ({ onPress }) => {
             .then((res) => {
                 if (res.status_code == 200) {
                 } else {
-                    // Alert.alert(res.message)
                 }
             })
     }
     useEffect(() => {
+        getMyId()
         getEventData()
     }, [])
     useEffect(() => {
@@ -56,54 +61,21 @@ const EventsCommponent = ({ onPress }) => {
         </View>
     );
     const list = ({ item }) => {
+        const AttendMembers = [item?.attend_users]
+        const NotAttendMembers = [item?.notAttend_users]
+        const AttendResult = AttendMembers[0]?.split(',')?.map(Number)?.includes(myid);
+        const NotAttendResult = NotAttendMembers[0]?.split(',')?.map(Number)?.includes(myid);
+
+        console.log(item);
+
         const formatDate = () => {
             const options = { day: '2-digit', month: 'short', year: 'numeric' };
             const date = new Date(item.event_date);
             return date.toLocaleDateString('en-GB', options).replace(',', '');
         };
-
-        const onNavigate = (i) => {
-            onPress(i)
-        }
-        console.log(item);
-
+        const onNavigate = (i) => { onPress(i) }
         return (
-            // <View style={{ height: 164, shadowOpacity: 0.3, shadowRadius: 4, shadowOffset: { height: 1, width: 1 }, backgroundColor: COLOR.white, marginTop: 12, flexDirection: 'row', alignItems: 'center', borderRadius: 15 }}>
-            //     <Image source={{ uri: item.event_image }} style={{ height: 144, width: 130, marginLeft: 10, borderRadius: 15 }} />
-            //     <View style={{ flex: 1, padding: 10 }}>
-            //         <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{item.event_title}</Text>
-            //         <Text style={{ width: '100%', fontSize: 11, lineHeight: 15, color: COLOR.gray, marginTop: 8 }}>{item.event_description.length > 80 ? item.event_description.slice(0, 80) + '...' : item.event_description}</Text>
 
-            //         <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between', marginTop: 15 }}>
-            //             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            //                 <Image source={require('../../Assets/Image/date.png')} style={{ height: 12, width: 12, tintColor: COLOR.green }} />
-            //                 <Text style={{ fontSize: 11, marginLeft: 5 }}>{formatDate()} </Text>
-            //             </View>
-            //             <View style={{ flexDirection: 'row', alignItems: 'center', }}>
-            //                 <Image source={require('../../Assets/Image/clock.png')} style={{ height: 12, width: 12, tintColor: COLOR.green }} />
-            //                 <Text style={{ fontSize: 11, marginLeft: 5 }}>{item.event_time} </Text>
-            //             </View>
-            //         </View>
-
-            //         <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between', marginTop: 10 }}>
-            //             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            //                 <Image source={require('../../Assets/Image/location.png')} style={{ height: 12, width: 12, tintColor: COLOR.green }} />
-            //                 <Text style={{ fontSize: 11, marginLeft: 5 }}>{item.location.length > 20 ? item.location.slice(0, 20) + '...' : item.location}</Text>
-            //             </View>
-            //             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            //                 <View>
-            //                     <FlatList
-            //                         data={item.usersArr}
-            //                         keyExtractor={(image, index) => index.toString()}
-            //                         renderItem={renderUserImage}
-            //                         horizontal
-            //                     />
-            //                 </View>
-            //                 <Text style={{ fontSize: 11, fontWeight: 'bold', color: COLOR.green }}>{item?.usersArr?.length - 3 <= 0 ? '' : ' +' + (item?.usersArr?.length - 3)}</Text>
-            //             </View>
-            //         </View>
-            //     </View>
-            // </View>
             <TouchableOpacity
                 onPress={() => { onNavigate(item) }}
                 style={{ height: 194, shadowOpacity: 0.3, shadowRadius: 4, shadowOffset: { height: 1, width: 1 }, backgroundColor: COLOR.white, marginTop: 12, flexDirection: 'row', borderRadius: 15, flex: 1, padding: 10 }}>
@@ -137,17 +109,28 @@ const EventsCommponent = ({ onPress }) => {
                         <Text style={{ fontSize: 11, fontWeight: 'bold', color: COLOR.green }}>{item?.usersArr?.length - 3 <= 0 ? '' : ' +' + (item?.usersArr?.length - 3)}</Text>
                     </View>
                 </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginTop: 15, marginHorizontal: 10, position: 'absolute', right: 0, bottom: 0, width: '65%' }}>
-                    <TouchableOpacity onPress={() => onHandalEvent(item, 'attend')} style={{ backgroundColor: COLOR.green, height: 30, borderRadius: 30, width: '41%', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', paddingHorizontal: 10, marginVertical: 10 }}>
-                        <Text style={{ color: COLOR.white, fontWeight: '600', fontSize: 14 }}>Attend</Text>
-                        <Image source={require('../../Assets/Image/accept.png')} style={{ height: 18, width: 18, resizeMode: 'contain', marginLeft: -2, tintColor: COLOR.white, marginLeft: 5 }} />
-                    </TouchableOpacity>
+                {item.created_by !== myid ? null :
+                    AttendResult ?
+                        <View style={{ backgroundColor: COLOR.lightgreen, paddingHorizontal: 10, padding: 5, borderRadius: 10, marginTop: 15, alignSelf: 'flex-start', alignItems: 'center', flexDirection: 'row', position: 'absolute', bottom: 10, right: 10 }}>
+                            <Image source={require('../../Assets/Image/approve.png')} style={{ height: 25, width: 25, marginRight: 5, }} />
+                            <Text style={{ fontSize: 15, color: COLOR.black, fontWeight: '600' }}>Attend</Text>
+                        </View> :
+                        NotAttendResult ? <View style={{ backgroundColor: COLOR.lightgreen, paddingHorizontal: 10, padding: 5, borderRadius: 10, marginTop: 15, alignSelf: 'flex-start', alignItems: 'center', flexDirection: 'row', position: 'absolute', bottom: 10, right: 10 }}>
+                            <Image source={require('../../Assets/Image/decline.png')} style={{ height: 30, width: 30, marginRight: 5, }} />
+                            <Text style={{ fontSize: 15, color: COLOR.orange, fontWeight: '600' }}>Not Attend</Text>
+                        </View> :
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginTop: 15, marginHorizontal: 10, position: 'absolute', right: 0, bottom: 0, width: '65%' }}>
+                                <TouchableOpacity onPress={() => onHandalEvent(item, 'attend')} style={{ backgroundColor: COLOR.green, height: 30, borderRadius: 30, width: '41%', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', paddingHorizontal: 10, marginVertical: 10 }}>
+                                    <Text style={{ color: COLOR.white, fontWeight: '600', fontSize: 14 }}>Attend</Text>
+                                    <Image source={require('../../Assets/Image/accept.png')} style={{ height: 18, width: 18, resizeMode: 'contain', marginLeft: -2, tintColor: COLOR.white, marginLeft: 5 }} />
+                                </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => onHandalEvent(item, 'notattend')} style={{ borderWidth: 1, borderColor: COLOR.green, height: 30, borderRadius: 30, width: '53%', flexDirection: 'row', paddingHorizontal: 10, justifyContent: 'space-around', alignItems: 'center' }}>
-                        <Text style={{ color: COLOR.green, fontSize: 14, fontWeight: '600' }}>Not Attend</Text>
-                        <Image source={require('../../Assets/Image/x.png')} style={{ height: 12, width: 12, resizeMode: 'contain', tintColor: COLOR.green, marginLeft: 5 }} />
-                    </TouchableOpacity>
-                </View>
+                                <TouchableOpacity onPress={() => onHandalEvent(item, 'notattend')} style={{ borderWidth: 1, borderColor: COLOR.green, height: 30, borderRadius: 30, width: '53%', flexDirection: 'row', paddingHorizontal: 10, justifyContent: 'space-around', alignItems: 'center' }}>
+                                    <Text style={{ color: COLOR.green, fontSize: 14, fontWeight: '600' }}>Not Attend</Text>
+                                    <Image source={require('../../Assets/Image/x.png')} style={{ height: 12, width: 12, resizeMode: 'contain', tintColor: COLOR.green, marginLeft: 5 }} />
+                                </TouchableOpacity>
+                            </View>}
+
 
             </TouchableOpacity>
         )
