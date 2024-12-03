835a4185851e5c3817ad99c914ef28f0cb0630a8
tec-sense <tecsense19@gmail.com>
2024-12-03 17:19:29 +0530

    Update MsgTask.js

--- Src/Screen/ChatInnerScreen/ChatCustomFile/MsgTask.js
+++ Src/Screen/ChatInnerScreen/ChatCustomFile/MsgTask.js
@@ -1,28 +1,35 @@
-import { View, Image, Text, TouchableOpacity, StyleSheet, Alert, FlatList } from 'react-native';
-import React from 'react';
-import { COLOR } from '../../../Assets/AllFactors/AllFactors';
+import { View, Image, Text, TouchableOpacity, StyleSheet, Modal, FlatList, TextInput, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { COLOR, HEIGHT, WIDTH } from '../../../Assets/AllFactors/AllFactors';
 import { MyID } from '../../../Service/AsyncStorage';
+import { BlurView } from '@react-native-community/blur';
+import Button from '../../../Custom/Button/Button';
+import { Add_Task_Comment } from '../../../Service/actions';
 
 const MsgTask = ({ data, ThreeDott }) => {
-
-
+    const [visible, setVisible] = useState(false)
+    const [comment, setComment] = useState('')
+    const [commentData, setCommentData] = useState('')
     const TaskData = data.messageDetails.tasks;
-    const user = data.messageDetails.users
-    // const users = task.task_checked_users.split(',').map(Number);
-    // console.log(data);
-    const imgData = [
-        { img: 'https://cdn.pixabay.com/photo/2024/06/24/04/05/woman-8849047_1280.jpg' },
-        { img: 'https://cdn.pixabay.com/photo/2024/06/24/04/05/woman-8849047_1280.jpg' },
-        { img: 'https://cdn.pixabay.com/photo/2024/06/24/04/05/woman-8849047_1280.jpg' },
-    ]
+
+
+    const AddComment = () => {
+        if (comment.trim() === '') {
+            return Alert.alert('Enter a Comment')
+        }
+        Add_Task_Comment(comment, commentData?.id, commentData?.message_id)
+            .then((res) => {
+                if (res.success == true) {
+                    setComment('')
+                    setVisible(false)
+                    setCommentData('')
+                }
+            })
+    }
     return (
-        // <TouchableOpacity onPress={onPress} disabled={disabled} style={styles.container}>
         <View style={styles.container}>
             <View style={styles.View1}>
-                <View style={{
-                    flexDirection: 'row',
-                    alignItems: 'center', marginBottom: 10
-                }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
                     <View style={styles.View2}>
                         <Text style={styles.taskTitle}>
                             {data?.messageDetails?.task_name?.length > 28
@@ -35,50 +42,82 @@ const MsgTask = ({ data, ThreeDott }) => {
                     </View>
                 </View>
 
-                {
-                    TaskData.map(async (task, index) => {
-                        const users = task.task_checked_users.split(',').map(Number);
-                        const myID = await MyID()
-                        return (
-                            <View key={index} style={styles.checkboxContainer}>
-                                <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
-                                    <View style={{ flexDirection: 'row', alignItems: 'center', width: '82%' }}>
-                                        <Image
-                                            source={users.includes(myID) ? require('../../../Assets/Image/check.png') : require('../../../Assets/Image/box.png')}
-                                            style={[styles.checkImg, { tintColor: users.includes(myID) ? COLOR.green : COLOR.black }]}
-                                        />
-                                        <Text numberOfLines={1} style={{ color: COLOR.gray, fontWeight: '600', flex: 1 }}>{task.checkbox}</Text>
-                                    </View>
-                                    <View style={{}}>
-                                        {/* <FlatList style={{}} horizontal data={imgData} renderItem={({ item, index }) => {
-                                            return (
-                                                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                                                    <Image source={{ uri: item.img }} style={{ height: 15, width: 15, borderRadius: 20, marginLeft: index == 0 ? 0 : - 5 }} />
-                                                </View>
-                                            )
-                                        }} /> */}
-                                    </View>
+                {TaskData.map(async (task, index) => {
+                    const users = task.task_checked_users.split(',').map(Number);
+                    const myID = await MyID()
+                    return (
+                        <View key={index} style={styles.checkboxContainer}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center', width: '82%' }}>
+                                    <Image source={users.includes(myID) ? require('../../../Assets/Image/check.png') : require('../../../Assets/Image/box.png')} style={[styles.checkImg, { tintColor: users.includes(myID) ? COLOR.green : COLOR.black }]} />
+                                    <Text numberOfLines={1} style={{ color: COLOR.gray, fontWeight: '600', flex: 1 }}>{task.checkbox}</Text>
                                 </View>
-                                {/* <FlatList style={{}} data={[{}, {}]} renderItem={({ item }) => {
-                                    return (
-                                        <View style={{ height: 40, flexDirection: 'row', alignItems: 'center', backgroundColor: '#F7F8F8', marginLeft: '10%', marginTop: 5, borderRadius: 5, paddingHorizontal: 10, }}>
-                                            <Image source={{ uri: 'https://cdn.pixabay.com/photo/2024/06/24/04/05/woman-8849047_1280.jpg' }} style={{ height: 20, width: 20, borderRadius: 20, marginRight: 5, }} />
-                                            <Text style={{ flex: 1, color: COLOR.gray, fontSize: 10 }} numberOfLines={2}>Lorem Ipsum Dummy TextLorem Ipsum Dummy TextLorem Ipsum Dummy TextLorem Ipsum Dummy Text</Text>
-                                        </View>
-                                    )
-                                }} /> */}
-                                {/* <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginLeft: '10%', marginTop: 5 }}>
-                                    <Image source={require('../../../Assets/Image/addcomment.png')} style={{ height: 12, width: 12, marginRight: 5 }} />
-                                    <Text style={{ fontSize: 12, color: COLOR.gray, fontWeight: '500' }}>Add Comment</Text>
-                                </TouchableOpacity> */}
+                                <View style={{}}>
+                                    <FlatList scrollEnabled={false} style={{}} horizontal data={task?.profiles} renderItem={({ item, index }) => {
+                                        return (
+                                            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                                                <Image source={{ uri: item.profile_url }} style={{ height: 15, width: 15, borderRadius: 20, marginLeft: index == 0 ? 0 : - 5 }} />
+                                            </View>
+                                        )
+                                    }} />
+                                </View>
                             </View>
-                        )
-                    })
+                            <FlatList scrollEnabled={false} style={{}} data={task.comments} renderItem={({ item }) => {
+                                return (
+                                    <View style={{ height: 40, flexDirection: 'row', alignItems: 'center', backgroundColor: '#F7F8F8', marginLeft: '10%', marginTop: 5, borderRadius: 5, paddingHorizontal: 10, }}>
+                                        <Image source={{ uri: item?.user?.profile_picture }} style={{ height: 20, width: 20, borderRadius: 20, marginRight: 5, }} />
+                                        <Text style={{ flex: 1, color: COLOR.gray, fontSize: 10 }} numberOfLines={2}>{item.comment}</Text>
+                                    </View>
+                                )
+                            }} />
+                            <TouchableOpacity
+                                onPress={() => { setVisible(true), setCommentData(task) }}
+                                style={{ flexDirection: 'row', alignItems: 'center', paddingHorizontal: '10%', paddingVertical: 10, alignSelf: 'flex-start' }}>
+                                <Image source={require('../../../Assets/Image/addcomment.png')} style={{ height: 14, width: 14, marginRight: 5 }} />
+                                <Text style={{ fontSize: 13, color: COLOR.gray, fontWeight: '500' }}>Add Comment</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )
+                })
                 }
             </View>
             <Text style={styles.tasktime}>{data?.time}</Text>
+            <Modal visible={visible} transparent>
+                <BlurView style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, alignItems: 'center', justifyContent: 'center' }}
+                    blurType="light"
+                    blurAmount={10} >
+                    <View style={{ width: '90%', backgroundColor: COLOR.white, padding: 20, borderRadius: 20, shadowOpacity: 0.3, shadowOffset: { height: 1, width: 1 }, shadowRadius: 10 }}>
+                        <TouchableOpacity onPress={() => setVisible(false)} style={{ alignSelf: 'flex-end', marginBottom: 20 }}>
+                            <Image source={require('../../../Assets/Image/x.png')} style={{ height: 15, width: 15 }} />
+                        </TouchableOpacity>
+                        <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: 'bold', }}>Add Comment</Text>
+                        <TextInput
+                            placeholder="Enter Comment . . ."
+                            onChangeText={res => setComment(res)}
+                            value={comment}
+                            multiline={true}
+                            placeholderTextColor={COLOR.lightgray}
+                            style={{
+                                fontSize: 16,
+                                fontWeight: '600',
+                                paddingVertical: 10,
+                                borderRadius: 5,
+                                marginTop: 20,
+                                height: 120,
+                                backgroundColor: COLOR.white,
+                                color: COLOR.black,
+                                paddingHorizontal: 10,
+                                shadowOpacity: 0.2,
+                                shadowOffset: { height: 1, width: 0 },
+                                shadowRadius: 3
+                            }}
+                        />
+
+                        <Button onPress={AddComment} marginTop={20} fontSize={16} title={'Add'} bgColor={COLOR.green} color={COLOR.white} />
+                    </View>
+                </BlurView>
+            </Modal>
         </View>
-        // </TouchableOpacity>
     );
 };
 
@@ -87,12 +126,10 @@ export default MsgTask;
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
-        // marginVertical: 5,
     },
     View1: {
         width: '100%',
         borderRadius: 10,
-        // paddingHorizontal: 10,
         backgroundColor: COLOR.white,
         shadowOpacity: 0.2,
         shadowOffset: { height: 1, width: 1 },
@@ -136,6 +173,6 @@ const styles = StyleSheet.create({
         fontSize: 12,
     },
     checkboxContainer: {
-        // marginTop: 20,
+        marginTop: 20,
     },
 });
