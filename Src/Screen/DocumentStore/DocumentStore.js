14e90e331b30687032340a26117fe19f5626073f
tec-sense <tecsense19@gmail.com>
2024-12-12 15:28:21 +0530

    Update DocumentStore.js

--- Src/Screen/DocumentStore/DocumentStore.js
+++ Src/Screen/DocumentStore/DocumentStore.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, Image, TouchableOpacity, Modal, TouchableWithoutFeedback } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity, Modal, TouchableWithoutFeedback, SafeAreaView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getToken } from '../../Service/AsyncStorage'
 import { Scan_Document_List } from '../../Service/actions'
@@ -6,6 +6,8 @@ import NavigateHeader from '../../Custom/Header/NavigateHeader'
 import { COLOR } from '../../Assets/AllFactors/AllFactors'
 import Button from '../../Custom/Button/Button'
 import Loader from '../../Custom/Loader/loader'
+import PDFView from 'react-native-view-pdf'
+import FastImage from 'react-native-fast-image'
 
 const DocumentStore = (props) => {
     const [documentData, setDocumentData] = useState([])
@@ -17,8 +19,9 @@ const DocumentStore = (props) => {
         const token = await getToken()
         Scan_Document_List(token)
             .then((res) => {
-                console.log(res.data);
-                setDocumentData(res.data)
+                const data = res.data
+                const pdfData = data?.filter((i) => i.attachment_name.toLowerCase().endsWith(".pdf"));
+                setDocumentData(pdfData)
                 setLoading(false)
             })
             .catch(() => {
@@ -27,7 +30,6 @@ const DocumentStore = (props) => {
     }
     const list = ({ item, index }) => {
         const date = new Date(item.created_at);
-
         const options = {
             day: '2-digit',
             month: 'short',
@@ -38,9 +40,15 @@ const DocumentStore = (props) => {
         };
         const formatter = new Intl.DateTimeFormat('en-US', options);
         const formattedDate = formatter.format(date);
+
+
+
         return (
+
             <TouchableOpacity onPress={() => { setVisible(true), setViewImage(item?.attachment_path) }} style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: COLOR.verylightgray, marginTop: 20, borderRadius: 5 }}>
-                <Image source={{ uri: item.attachment_path }} style={{ height: 60, width: 60, borderRadius: 5 }} />
+                {/* <Image
+                    source={{ uri: item.attachment_path }} style={{ height: 60, width: 60, borderRadius: 5 }} /> */}
+                <FastImage style={{ height: 60, width: 60 }} source={item.attachment_path} />
                 <View>
                     <Text style={{ color: COLOR.black, fontSize: 16, fontWeight: '700', marginLeft: 10 }}>{item.attachment_name?.length > 25 ? item?.attachment_name.slice(0, 28) + '...' : ''}</Text>
                     <Text style={{ color: COLOR.gray, fontSize: 12, fontWeight: '700', marginTop: 5, marginLeft: 10 }}>{formattedDate}</Text>
@@ -58,7 +66,7 @@ const DocumentStore = (props) => {
                 <NavigateHeader title={'Documents'} onPress={() => props.navigation.goBack()} />
             </View>
             {documentData.length > 0 ?
-                <FlatList bounces={false} data={documentData} renderItem={list} style={{ marginHorizontal: 15 }} />
+                <FlatList bounces={false} data={documentData} renderItem={list} style={{ paddingHorizontal: 15 }} />
                 : <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                     <Image source={{ uri: 'https://cdn4.iconfinder.com/data/icons/backyard-music/96/Audio-Music-Listen-Player-28-512.png' }} style={{ height: 50, width: 50, tintColor: COLOR.gray, marginBottom: 10 }} />
                     <Text style={{ fontSize: 18, fontWeight: '700', color: COLOR.gray, marginBottom: 50 }}>{'No Document!'}</Text>
@@ -74,9 +82,29 @@ export default DocumentStore
 const ViewImageModal = ({ visible, URI, onLoad, onPressClose }) => {
     return (
         <Modal transparent visible={visible}>
-            <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.3)' }}>
-                <Image source={{ uri: URI }} style={{ height: 400, marginTop: 200, borderRadius: 10 }} onLoad={onLoad} />
-                <Button onPress={onPressClose} color={COLOR.white} title={'Close'} borderColor={COLOR.white} borderWidth={1} marginHorizontal={'35%'} marginTop={20} />
+            <View style={{ flex: 1, backgroundColor: COLOR.black }}>
+                {/* <Image source={{ uri: URI }} style={{ height: 400, marginTop: 200, borderRadius: 10 }} onLoad={onLoad} />
+                <Button onPress={onPressClose} color={COLOR.white} title={'Close'} borderColor={COLOR.white} borderWidth={1} marginHorizontal={'35%'} marginTop={20} /> */}
+                <View style={{ paddingHorizontal: 20 }}>
+                    <NavigateHeader
+                        onPress={onPressClose}
+                        color={COLOR.white}
+                        title={'Pdf View'}
+                        top={10}
+                    />
+                </View>
+                <View style={{ flex: 1, borderTopRightRadius: 20, borderTopLeftRadius: 20, backgroundColor: COLOR.verylightgray, }}>
+                    <PDFView
+                        onLoad={(a) => {
+                            console.log('load');
+                        }}
+
+                        resourceType={'url'}
+                        resource={URI}
+                        style={{ flex: 1, }}>
+                    </PDFView>
+                </View>
+
             </View>
 
         </Modal>
