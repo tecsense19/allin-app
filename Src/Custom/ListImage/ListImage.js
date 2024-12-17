b10183f08e896e63650c78ba32557147f5b5deb3
tec-sense <tecsense19@gmail.com>
2024-12-17 15:25:56 +0530

    Update ListImage.js

--- Src/Custom/ListImage/ListImage.js
+++ Src/Custom/ListImage/ListImage.js
@@ -1,10 +1,10 @@
 import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 
-const ListImage = ({ uri, marginRight }) => {
+const ListImage = ({ uri, marginRight, height, width, marginLeft }) => {
     return (
-        <ImageBackground source={require('../../Assets/Image/admin.jpg')} style={{ height: 55, width: 55, borderRadius: 55, marginRight: marginRight ? marginRight : 10 }}>
-            <Image source={{ uri: uri }} style={{ width: '100%', height: '100%', resizeMode: 'cover', borderRadius: 55 }} />
+        <ImageBackground source={require('../../Assets/Image/admin.jpg')} style={{ height: height ? height : 55, width: width ? width : 55, borderRadius: 55, marginRight: marginRight ? marginRight : 10, marginLeft: marginLeft ? marginLeft : 0 }}>
+            <Image source={{ uri: uri }} style={{ height: height ? height : 55, width: width ? width : 55, resizeMode: 'cover', borderRadius: 55, }} />
         </ImageBackground>
     )
 }
